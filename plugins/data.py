e6f83162cb3ead52ddcd9810e116bba19f73bd69
Girish019 <118633556+Girish019@users.noreply.github.com>
2023-12-16 18:54:46 +0530

    Update data.py

--- plugins/data.py
+++ plugins/data.py
@@ -35,6 +35,7 @@ Elements in <b>"{}{}"</b>
 ############{'serial name':["pic", "short link domin", "short link api", "To channel id"]}###############
 #ex =>>>    '':['','','','']
 DATAODD = {
+ #zee noon 
         'Rajeshwari_Vilas_Coffee_Club_':['https://graph.org/file/8fb299e1497c166065892.jpg','','','-1002126345865'],
         'Seethe_Ramudi_Katnam_':['https://graph.org/file/517d41e6bda6f38ce7730.jpg','','','-1002126345865'],
         'Mukkupudaka_':['https://graph.org/file/b894f656bbc1f1dd477b4.jpg','','','-1002126345865'],
@@ -42,7 +43,7 @@ DATAODD = {
         'Jabilli_Kosam_Aakashamalle_':['https://graph.org/file/67b5969a42a3b47ab0754.jpg','','','-1002126345865'],
         'Subhasya_Seeghram_':['https://graph.org/file/047e212145409524e7ec8.jpg','','','-1002126345865'],
         'Oohalu_Gusagusalade_':['https://graph.org/file/a289d5007caaeebe4b18e.jpg','','','-1002126345865'],
-  #noon
+  #zee evening 
         'Radhamma_Kuthuru_':['https://graph.org/file/9934bd7b3c3492dd01e42.jpg','','','-1002126345865'],
         'Chiranjeevi_Lakshmi_Sowbhagyavati_':['https://graph.org/file/f75069b1b3a99f5652e70.jpg','','','-1002126345865'],
         'Nindu_Noorella_Saavasam_':['https://graph.org/file/a4005c14fd51c5a32cc2c.jpg','','','-1002126345865'],
@@ -55,6 +56,7 @@ DATAODD = {
   
 }
 DATAEVEN = {
+ #zee noon
         'Rajeshwari_Vilas_Coffee_Club_':['https://graph.org/file/8fb299e1497c166065892.jpg','','','-1002126345865'],
         'Seethe_Ramudi_Katnam_':['https://graph.org/file/517d41e6bda6f38ce7730.jpg','','','-1002126345865'],
         'Mukkupudaka_':['https://graph.org/file/b894f656bbc1f1dd477b4.jpg','','','-1002126345865'],
@@ -62,7 +64,7 @@ DATAEVEN = {
         'Jabilli_Kosam_Aakashamalle_':['https://graph.org/file/67b5969a42a3b47ab0754.jpg','','','-1002126345865'],
         'Subhasya_Seeghram_':['https://graph.org/file/047e212145409524e7ec8.jpg','','','-1002126345865'],
         'Oohalu_Gusagusalade_':['https://graph.org/file/a289d5007caaeebe4b18e.jpg','','','-1002126345865'],
-  #noon
+  #zee evening 
         'Radhamma_Kuthuru_':['https://graph.org/file/9934bd7b3c3492dd01e42.jpg','','','-1002126345865'],
         'Chiranjeevi_Lakshmi_Sowbhagyavati_':['https://graph.org/file/f75069b1b3a99f5652e70.jpg','','','-1002126345865'],
         'Nindu_Noorella_Saavasam_':['https://graph.org/file/a4005c14fd51c5a32cc2c.jpg','','','-1002126345865'],
