46c6d383b8cfc244262dcbef5abefb04364549d9
Girish019 <118633556+Girish019@users.noreply.github.com>
2023-12-16 15:52:58 +0530

    Update data.py

--- plugins/data.py
+++ plugins/data.py
@@ -31,21 +31,24 @@ Elements in <b>"{}{}"</b>
 ############{'serial name':["pic", "short link domin", "short link api", "To channel id"]}###############
 #ex =>>>    '':['','','','']
 DATAODD = {
-        'Radhaku_Neevera_Praanam_':['https://graph.org/file/dc119c8d45919db61181c.jpg','tnvalue.in','ff426552eda72230153ea3450a0bce0557183ccb','-1001942664190'],
-        'Gundamma_Katha_':['https://graph.org/file/baff21a920cc09f0ba89a.jpg','moneycase.link','0c2406a0cf1b54db75fcba4cbb8e389bdc675eee','-1001942664190'],
-        'Rajeshwari_Vilas_Coffee_Club_':['https://graph.org/file/15328d049e6d89a06030a.jpg','links4money.com','668f8a8d31c4d5e55da03d0d21ae740f3caf5d14','-1001942664190'],
-        'Ammayi_Garu_':['https://graph.org/file/2afa02552352eb1ad1654.jpg','links4money.com','668f8a8d31c4d5e55da03d0d21ae740f3caf5d14','-1001942664190'],
-        'Oohalu_Gusagusalade_':['https://graph.org/file/66f92045eece717617ac1.jpg','links4money.com','ba4f1e7f7cfbea8b02c4c031be2270e354e95d47','-1001942664190'],
-        'Maa_Varu_Mastaaru_':['https://graph.org/file/0c7b76e2ffba37dc80f24.jpg','tnvalue.in','e496ab76b42f51628da20bb69577397533008f3c','-1001942664190'],
-        'Chiranjeevi_Lakshmi_Sowbhagyavati_':['https://graph.org/file/2a210b8e8c9c008a21626.jpg','moneycase.link','bdc62bf7dd54515abf15371b58feb6a1ff2b434a','-1001942664190'],
-        'Seethe_Ramudi_Katnam_':['https://graph.org/file/40879ee73f036312ebb91.jpg','tnshort.net','cfafc72b0df2558f0f5ad4e1c906ce9f783281e3','-1001942664190'],
-        'Jagadhatri_':['https://graph.org/file/ea7b3f138cbb1cbf89328.jpg','linkshortify.com','9e82f5e0d917c633234194f39c2985752efd67e0','-1001942664190'],
-        'Chiranjeevi_Lakshmi_Sowbhagyavati_':['https://graph.org/file/979b1125f1b333d1b8b07.jpg','links4money.com','f2c99e727ba0a517e53f77a31b4f79d8acfc318c','-1001942664190'],
-        'Trinayani_':['https://graph.org/file/db548886e1746a102b62a.jpg','links4money.com','ba4f1e7f7cfbea8b02c4c031be2270e354e95d47','-1001942664190'],
-        'Radhamma_Kuthuru_':['https://graph.org/file/edffb4f77446259b8ca5e.jpg','tnshort.net','0045de7eda3a082a38f05bdd0d1c4de1262709fb','-1001942664190'],
-        'Prema_Entha_Maduram_':['https://graph.org/file/875789bddf3b18111a8b1.jpg','linkshortify.com','8a9a3df5db074b2b1ad10efcd383e5243118447b','-1001942664190'],
-        'Nindu_Noorella_Saavasam_':['https://graph.org/file/6bf07ca5c1fd598b73478.jpg','moneycase.link','bdc62bf7dd54515abf15371b58feb6a1ff2b434a','-1001942664190'],
-        'Subhasya_Seeghram_':['https://graph.org/file/9a8a693eca9c41a57bade.jpg','moneycase.link','f35b0c3ac535a200b3287c698b652e58b4689b6f','-1001942664190'],
+        'Rajeshwari_Vilas_Coffee_Club_':['','','',''],
+        'Seethe_Ramudi_Katnam_':['','','',''],
+        'Mukkupudaka_':['','','',''],
+        'Gundamma_Katha_':['','','',''],
+        'Jabilli_Kosam_Aakashamalle_':['','','',''],
+        'Subhasya_Seeghram_':['','','',''],
+        'Oohalu_Gusagusalade_':['','','',''],
+  #noon
+        'Radhamma_Kuthuru_':['','','',''],
+        'Chiranjeevi_Lakshmi_Sowbhagyavati_':['','','',''],
+        'Nindu_Noorella_Saavasam_':['','','',''],
+        'Jagadhatri_':['','','',''],
+        'Padamati_Sandhyaragam_':['','','',''],
+        'Trinayani_':['','','',''],
+        'Prema_Entha_Maduram_':['','','',''],
+        'Ammayi_Garu_':['','','',''],
+        'Suryakantham_':['','','','']
+  
 }
 DATAEVEN = {
         'Radhaku_Neevera_Praanam_':['https://graph.org/file/dc119c8d45919db61181c.jpg','moneycase.link','0c2406a0cf1b54db75fcba4cbb8e389bdc675eee','-1001942664190'],
