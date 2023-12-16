c5144bfb4092887ecbe441e3ad59abe75228d122
Girish019 <118633556+Girish019@users.noreply.github.com>
2023-12-16 18:52:07 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -85,7 +85,7 @@ async def channel_post(client: Client, message: Message):
     await asyncio.sleep(0.5)
     await client.send_photo(chat_id=chtid, photo=pic, caption=FOMET.format(DATEDAY[-1], Size, Eno[0], Tlink, Tlink))
     await asyncio.sleep(1)
-    await bot_msg.edit(BOTEFITMSG.format(filname, botfsno[0], Tlink, Size, DATEDAY[-1]))
+    await bot_msg.edit(BOTEFITMSG.format(filname, Eno[0], Tlink, Size, DATEDAY[-1]))
     # await bot_msg.edit(BOTEFITMSG.format(filname, botfsno[0], Tlink, Slink, Size, DATEDAY[-1])) #msg edit to "please wait...(see line 39" msg ==> and finally the elements belongs to sent serials are updated here
     #await e_pic.edit) # msg edit in forwarder channel = "pic without captions (see line 41)" ==> thats return to our given format and short link ,date are updated here
 
